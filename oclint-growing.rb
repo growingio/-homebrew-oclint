53da68f1354a1459ff1551d89cb2659dc78589a5
好菜好菜 <277715243@qq.com>
2020-08-07 11:29:17 +0800

    Update oclint-growing.rb

--- oclint-growing.rb
+++ oclint-growing.rb
@@ -11,10 +11,6 @@ class OclintGrowing < Formula
   # depends_on "cmake" => :build
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
     clang_version = '10.0.0'
 
     include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
